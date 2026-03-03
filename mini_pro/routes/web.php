32db97c6ff0280e07a7dece00dce30f68781c937
2211844085 <mrxly12345@gmail.com>
2026-03-03 05:32:47 +0200

    a

--- mini_pro/routes/web.php
+++ mini_pro/routes/web.php
@@ -2,13 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< Updated upstream
 use App\Http\Controllers\BookController;
-
-=======
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
->>>>>>> Stashed changes
 Route::get('/', function () {
     return view('welcome');
 });
